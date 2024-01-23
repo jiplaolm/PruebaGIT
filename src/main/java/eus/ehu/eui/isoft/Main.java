bdab85e851f8d83f903845f960ed92619daa066b
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2024-01-23 17:24:54 +0100

    Utilizamos el método1

--- src/main/java/eus/ehu/eui/isoft/Main.java
+++ src/main/java/eus/ehu/eui/isoft/Main.java
@@ -3,6 +3,8 @@ package eus.ehu.eui.isoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Llamamos al metodo 1
+        method1();
     }
 
     public static void method1() {
