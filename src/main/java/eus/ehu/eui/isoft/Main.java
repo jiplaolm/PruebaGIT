b65714fda251d116961ccd0367fb8463f35d9f89
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2024-01-23 17:23:20 +0100

    Creado metodo 2 en la rama de pruebas

--- src/main/java/eus/ehu/eui/isoft/Main.java
+++ src/main/java/eus/ehu/eui/isoft/Main.java
@@ -3,9 +3,15 @@ package eus.ehu.eui.isoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Llamada al metodo 2
+        method2();
     }
 
     public static void method1() {
         System.out.println("Esto es un método");
+    }
+
+    public static void method2() {
+        System.out.println("Otro método");
     }
 }
